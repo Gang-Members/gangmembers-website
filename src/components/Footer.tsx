6bd8dbf54ee0d1f6d40d2352d642c3d99d7cd210
Sebastiaan <Sebastiaan.denhertog@gmail.com>
2024-03-26 09:47:38 +0100

    Footer update with icon placeholders and copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,18 @@ import React from "react";
 export default function footer() {
     return (
         <footer>
-                <div>
-                    Footer
-                </div>
+            <section>
+                <ul>
+                    <li><a href="https://www.x.com"><img src="https://via.placeholder.com/50" alt="x" /></a></li>
+                    <li><a href="https://www.instagram.com"><img src="https://via.placeholder.com/50" alt="instagram" /></a></li>
+                    <li><a href="https://www.youtube.com"><img src="https://via.placeholder.com/50" alt="youtube" /></a></li>
+                    <li><a href="https://www.mail.com"><img src="https://via.placeholder.com/50" alt="mail" /></a></li>
+                    <li><a href="https://www.steam.com"><img src="https://via.placeholder.com/50" alt="steam" /></a></li>
+                </ul>
+            </section>
+            <section>
+                <p> Created by GangMembers &copy; 2024</p>
+            </section>
         </footer>
     );
 };
